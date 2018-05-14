bccc32c9edc6aacd6884214a81fa2e84917c5b19
rocky.shimithy <rocky.ribeiro@tivit.com.br>
2018-05-14 17:36:08 -0300

    Added tests

---
+++ tests/unit/test_api_manager.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+
+
+def test_get_employees():
+    pass
+
+def test_get_employee():
+    pass
+
+def test_post_employees():
+    pass
+
+def test_delete_employees():
+    pass
